7151307e97d9349b3eeebb0e4ee062c5a89d5f9e
moeidsaleem <moeidsaleem@gmail.com>
2018-10-26 00:09:08 +0500

    asdad

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const app =express()
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.static(__dirname +'/dist'));
 
-const app = express();
 
 
 
